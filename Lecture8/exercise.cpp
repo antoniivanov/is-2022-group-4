70f6af6dde8dd7de1f728f457da721f5a6c50085
Antoni Ivanov <aivanov@vmware.com>
2022-11-28 00:09:45 +0200

    add tasks

--- Lecture8/exercise.cpp
+++ Lecture8/exercise.cpp
@@ -1,22 +1,10 @@
-
-## 1 
-
-Given a string ,
- write a function to examine whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in the given string
-
-The algorithm is the following: 
-
-![x](https://user-images.githubusercontent.com/2536458/204161040-4a09f40f-3294-4e71-8453-85a6290109e0.jpg)
-
-```
-bool are_brackets_balanced(std::string expr) {
+ bool are_brackets_balanced(std::string expr) {
     std::stack<char> stack;
 }
-```
 
-## 2 Moving average 
 
-```
+
+
 class MovingAverage {
 private: 
   int window_size;
@@ -34,9 +22,3 @@ public:
     return /* the new moving average*/
   }
 }
-```
-
-* Calculate complexity 
-* What if we can use deque (double ended queue) . How the solution and calcualted complexity changes 
-* What if it's ciruclar queue. 
-
