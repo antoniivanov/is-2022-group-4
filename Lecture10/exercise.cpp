006cac8823fb04ca33014b80f20bb36de324f983
Antoni Ivanov <aivanov@vmware.com>
2022-12-12 22:27:32 +0200

    xxx

--- Lecture10/exercise.cpp
+++ Lecture10/exercise.cpp
@@ -26,53 +26,59 @@ private:
 
     Node< std::pair<K, V> > *root;
 
-    
 
     Node< std::pair<K,V> >* get_node_and_parent_iterative(
-                    Node< std::pair<K,V> >* root, 
-                    K key, 
+                    Node< std::pair<K,V> >* root_node, 
+                    const K& key, 
                     Node< std::pair<K,V> > **parent_node)
     {
-        Node< std::pair<K,V> >* parent = NULL;
-        Node< std::pair<K,V> >* current = root;
+        Node< std::pair<K,V> >* current = root_node;
 
         while (current != nullptr) {
+            
             if (current->data.first > key) {
-                parent = current;
+                if (parent_node != nullptr) {
+                    *parent_node = current;
+                }
                 current = current->left;
             }
             else if (current->data.first < key) {
-                parent = current;
-                current = current->right;
-            } if (current->data.first == key) {
                 if (parent_node != nullptr) {
-                    *parent_node = parent;
+                    *parent_node = current;
                 }
+                current = current->right;
+            }
+            else if (current->data.first == key) {
                 return current;
             }
+
         }
         return nullptr;
     }
  
 
 
-    Node< std::pair<K,V> >* add_helper (Node< std::pair<K,V> >* root, std::pair<K,V> data) {
-        if (root == nullptr) {
+    Node< std::pair<K,V> >* add_helper (Node< std::pair<K,V> >* root_node, std::pair<K,V> data) {
+        if (root_node == nullptr) {
             return new Node< std::pair<K,V> >(data, nullptr, nullptr);
         } 
-        auto value = root.data;
-        if (data.first > root.data.first) {
-            root->right = add_helper(root->right, value);
-        } else if (data.first < root.data.first) {
-            root->left = add_helper(root->left, value);
+        auto value = root_node.data;
+        if (data.first > root_node.data.first) {
+            root_node->right = add_helper(root_node->right, value);
+        } else if (data.first < root_node.data.first) {
+            root_node->left = add_helper(root_node->left, value);
         } else {
-            root->data = data;
+            root_node->data = data;
         }
      
-        return root;
+        return root_node;
     }
 
 public:
+
+    Dictionary() {
+        this->root = nullptr;
+    }
 
     void add(K key, V value) {
         std::pair<K,V> data (key, value);
@@ -82,7 +88,7 @@ public:
         if (this->root == nullptr) {
             this->root = new Node< std::pair<K,V> >(data, nullptr, nullptr);
         } else {
-            Node<std::pair<K,V> >* parent;
+            Node<std::pair<K,V> >* parent = nullptr;
             auto found = this->get_node_and_parent_iterative(this->root, key, &parent);
             if (found == nullptr) {
                 if(parent->data.first > key) {
@@ -118,11 +124,13 @@ public:
 };
 
 void test_contains_using_empty_dictionary() {
+    std::cerr << "test_contains_using_empty_dictionary\n";
     Dictionary<std::string, int> d;
     assert (d.contains("foo") == false);
 }
 
 void test_contains_using_few_elements_not_found() {
+    std::cerr << "test_contains_using_few_elements_not_found\n";
     Dictionary<std::string, int> d;
     d.add("other", 1);
     d.add("other2", 2);
@@ -130,16 +138,17 @@ void test_contains_using_few_elements_not_found() {
 }
 
 void test_contains_using_few_element_found() {
+    std::cerr << "test_contains_using_few_element_found\n";
     Dictionary<std::string, int> d;
     d.add("foo", 1);
     d.add("other2", 2);
-    std::cerr << "vlaue " << d.get_value("foo"); 
-    //assert (d.contains("foo") == true);
+    assert (d.contains("foo") == true);
 }
 
 int main()
 {
-    std::cerr<<"Hello World";
+    test_contains_using_empty_dictionary();
+    test_contains_using_few_elements_not_found();
     test_contains_using_few_element_found();
 
     return 0;
